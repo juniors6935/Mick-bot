7ffe6257545a9a470c39ea6f848c6e5e839b0f4a
juniors6935 <tchamenjunior@gmail.com>
2026-08-07 12:31:19 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  Number: "243833389567" // remplacez par votre numéro WhatsApp sans mettre le signe +
+  Number: "380680068637" // remplacez par votre numéro WhatsApp sans mettre le signe +
 };
